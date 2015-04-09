f1fc29ef26a797034de3c953c025d8b29f3d9b84
kawasima <kawasima1016@gmail.com>
2015-04-10 01:18:21 +0900

    Added example with parameters.

---
+++ src/main/java/example/param/ParameterizedBatchlet.java
@@ -0,0 +1,30 @@
+package example.param;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.batch.api.AbstractBatchlet;
+import javax.batch.runtime.context.JobContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+
+@Named
+public class ParameterizedBatchlet extends AbstractBatchlet{
+    private static final Logger LOG = LoggerFactory.getLogger("job-streamer");
+
+    @Inject
+    JobContext jobContext;
+
+    @Override
+    public String process() throws Exception {
+        Properties jobProperties = jobContext.getProperties();
+        LOG.info("jobProperties=" + jobProperties);
+        String dateStr = jobProperties.getProperty("date");
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse(dateStr);
+        LOG.info("Argument['date']=" + date);
+        return "Processed";
+    }
+}
