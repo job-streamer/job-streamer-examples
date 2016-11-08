21b29d433a6fd7c147d3c7f0760934fbdf6f6673
Yuki Seki <schokolade0317@gmail.com>
2016-11-08 13:28:04 +0900

    change how to interap

--- src/main/java/example/StoppableFiveMinutesBatchlet.java
+++ src/main/java/example/StoppableFiveMinutesBatchlet.java
@@ -1,28 +1,38 @@
 package example;
 
-import java.util.concurrent.TimeUnit;
+import javax.batch.api.AbstractBatchlet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Sleep 5 minutes.
+ * Sleep 5 minutes It can stop.
  * 
- * @author kawasima
+ * @author Yuki Seki
  */
-public class StoppableFiveMinutesBatchlet extends AbstractStoppableBatchlet {
-    private static final Logger LOG = LoggerFactory.getLogger(AbstractStoppableBatchlet.class);
+public class StoppableFiveMinutesBatchlet extends AbstractBatchlet {
+    private static final int THREAD_SLEEP = 1000;
+    private static final int MINUTES_TO_MS = 60000;
+    private static final Logger LOG = LoggerFactory.getLogger(StoppableFiveMinutesBatchlet.class);
+    private volatile boolean running;
 
-    @Override
-    public void containerProcess() throws Exception{
-        for (int i = 0; i < 5; i++) {
-            TimeUnit.MINUTES.sleep(1);
-            LOG.info(i + "minutes.");
+    @Override public String process() throws Exception {
+        running = true;
+        int threadSleapCount = 0;
+        int minutes = 0;
+        while (running) {
+            Thread.sleep(THREAD_SLEEP);
+            threadSleapCount++;
+            if(threadSleapCount % MINUTES_TO_MS == 0){
+                minutes++;
+                LOG.info("It takes {} minutes", minutes);
+            }
         }
+        return null;
     }
 
     @Override
-    public void whenStop() {
-        LOG.info("stopped.");
+    public void stop() throws Exception {
+        running = false;
     }
 }
